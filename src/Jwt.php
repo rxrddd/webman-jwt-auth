51c4e364cb468b80db5bd570f205cdd29a78bd95
邹江勇 <zoujiangyong@126.com>
2024-11-21 20:32:45 +0800

    fix:支持8.1+版本 create of dynamic property Jwt::$jwtConfiguration is deprecated 的问题

--- src/Jwt.php
+++ src/Jwt.php
@@ -18,6 +18,7 @@ class Jwt
 {
 
     public Token $token;
+    private Configuration $jwtConfiguration;
 
     public function __construct(protected JwtAuth $auth,protected $config,protected $manager)
     {
